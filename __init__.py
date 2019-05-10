10b6f21b063a2c7320403cb543b48b7e44df3259
TolcheevaAlena <tolcheevaal@gmail.com>
2019-05-10 13:46:59 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,9 @@
 import telebot
 
 VSU_bot = telebot.TeleBot('716129192:AAHlKIXNx7Y8JiQF_c5hevXMx1e7j6wTClo')
+
+
+
 keyboard1 = telebot.types.ReplyKeyboardMarkup()
 keyboard1.row('Подписаться на рассылку', 'Выбрать вопрос из шаблона', 'Получить развернутый ответ на вопрос')
    
